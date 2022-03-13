ce5d797845c7bcf94848b84a2e5f95335962243b
YK854 <95330893+YK854@users.noreply.github.com>
2022-03-13 12:52:50 +0000

    [add]復習4

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '5ea845b5ca63157b1e6f512fe6175f597d9dbdff8c8d655061a3d2fe2f2462202fdc0d0404e3ecf8d8b6b61023688b62201744199dddbb79cf7fd52a622f7c4e'
